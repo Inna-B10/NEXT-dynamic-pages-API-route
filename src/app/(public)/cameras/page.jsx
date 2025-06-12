aedc98b9e61b9ab4017cfd60b9c31805dbdbb3e5
Inna-B10 <inna.b10.kodehode@gmail.com>
2025-06-12 11:19:48 +0200

    feat: added react-query for infinite scrolling in Cameras page; refactor data fetching logic

--- src/app/(public)/cameras/page.jsx
+++ src/app/(public)/cameras/page.jsx
@@ -1,36 +1,63 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useEffect, useRef } from 'react'
+import { useInfiniteQuery } from '@tanstack/react-query'
 import { CameraCard } from '@/components/CameraCard'
 import { LIMIT } from '@/constants/constants'
 import { camerasService } from '@/services/client/cameras.service'
 
 export default function CamerasPage() {
-	const [data, setData] = useState([])
-	const [page, setPage] = useState(0)
+	const observerRef = useRef()
+	const { data, isLoading, isError, fetchNextPage, hasNextPage, isFetchingNextPage } =
+		useInfiniteQuery({
+			queryKey: ['get_all_cameras'],
+			queryFn: ({ pageParam = 0 }) =>
+				camerasService.getPreviewItems({ limit: LIMIT, offset: pageParam }),
+			getNextPageParam: (lastPage, pages) => {
+				const loaded = pages.length * LIMIT
+				return loaded < lastPage.total ? loaded : undefined
+			}
+		})
 
 	useEffect(() => {
-		async function fetchData() {
-			const limit = LIMIT
-			const offset = page * limit
+		if (!observerRef.current || !hasNextPage) return
 
-			const response = await camerasService.getPreviewItems({ limit, offset }).catch(console.error)
-			if (response) setData(response.items)
-		}
-		fetchData()
-	}, [page])
+		const observer = new IntersectionObserver(entries => {
+			if (entries[0].isIntersecting) {
+				fetchNextPage()
+			}
+		})
+		observer.observe(observerRef.current)
+		return () => observer.disconnect()
+	}, [observerRef.current, hasNextPage])
+
+	if (isLoading) return <p>Loading...</p>
+	if (isError) return <p>Error loading data</p>
+
+	const allItems = data?.pages.flatMap(page => page.items) || []
 
 	return (
 		<>
-			{data && (
-				<div className='grid grid-cols-4 gap-y-14 gap-x-8'>
-					{data.map((item, index) => (
-						<CameraCard
-							key={index}
-							item={item}
-							index={index}
-						/>
-					))}
+			{allItems?.length ? (
+				<>
+					<div className='grid grid-cols-4 gap-y-14 gap-x-8'>
+						{allItems?.map((item, index) => (
+							<CameraCard
+								key={index}
+								item={item}
+								index={index}
+							/>
+						))}
+					</div>
+					{isFetchingNextPage && <p>Loading next page...</p>}
+					<div
+						ref={observerRef}
+						style={{ height: 1, marginTop: 180 }}
+					/>
+				</>
+			) : (
+				<div>
+					<p>No product in this category.</p>
 				</div>
 			)}
 		</>
--- src/providers/Providers.jsx
+++ src/providers/Providers.jsx
@@ -1,12 +1,30 @@
 'use client'
 
+import { useState } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { LazyMotion, domAnimation } from 'framer-motion'
 import { SidebarProvider } from './SidebarProvider'
 
 export function Providers({ children }) {
+	const [queryClient] = useState(
+		() =>
+			new QueryClient({
+				defaultOptions: {
+					queries: {
+						retry: 1,
+						refetchOnWindowFocus: false
+					},
+					mutations: {
+						retry: 1
+					}
+				}
+			})
+	)
 	return (
-		<LazyMotion features={domAnimation}>
-			<SidebarProvider>{children}</SidebarProvider>
-		</LazyMotion>
+		<QueryClientProvider client={queryClient}>
+			<LazyMotion features={domAnimation}>
+				<SidebarProvider>{children}</SidebarProvider>
+			</LazyMotion>
+		</QueryClientProvider>
 	)
 }
