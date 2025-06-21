d46b4d9448abec2f6ed485a8d06f2d1480049382
Inna-B10 <inna.b10.kodehode@gmail.com>
2025-06-21 03:05:17 +0200

    feat: added InfoRow, Rating, PriceShoppingButtonBlock, RatingBlock, and ShareFavoritesBlock components for product page

---
+++ src/components/product-page/blocks/PriceShoppingButtonBlock.jsx
@@ -0,0 +1,17 @@
+import { ShoppingCart } from 'react-feather'
+import { Button } from '../../ui/buttons/Button'
+
+export function PriceShoppingButtonBlock() {
+	return (
+		<div className='w-full flex flex-col gap-4 items-center pb-2 lg:w-1/2 lg:h-full lg:gap-6 lg:pb-0 lg:border-r lg:pr-4 border-dashed border-border'>
+			<h3 className='text-3xl lg:text-4xl font-bold text-yellow font-nanum'>5 995,-</h3>
+			<Button
+				title='Add to shopping cart'
+				aria-label='Add to shopping cart'
+			>
+				<ShoppingCart className='w-5 h-5 sm:w-6 sm:h-6' />
+				Add to cart
+			</Button>
+		</div>
+	)
+}
---
+++ src/components/product-page/blocks/RatingBlock.jsx
@@ -0,0 +1,25 @@
+import { Rating } from '../Rating'
+
+export default function RatingBlock({ variant, ratingData }) {
+	if (variant === 'bigScreen') {
+		/* -------------------------------- BigScreen ------------------------------- */
+		return (
+			<div className='hidden lg:flex flex-col w-1/4 h-full bg-bgSecondary rounded-md p-8'>
+				<h2 className='text-2xl font-bold text-yellow mb-6'>Rating:</h2>
+				<div className='flex flex-col gap-4 justify-between items-center w-full'>
+					<Rating ratings={ratingData} />
+				</div>
+			</div>
+		)
+	} else {
+		/* --------------------------- Small/Medium Screen -------------------------- */
+		return (
+			<div className='w-full bg-bgSecondary rounded-md p-4 sm:w-1/2 sm:p-0 sm:pr-4 sm:border-r border-dashed border-border'>
+				<h2 className='text-xl sm:text-lg md:text-xl font-bold text-yellow mb-2'>Rating:</h2>
+				<div className='flex flex-col gap-4 justify-between items-center w-full'>
+					<Rating ratings={ratingData} />
+				</div>
+			</div>
+		)
+	}
+}
