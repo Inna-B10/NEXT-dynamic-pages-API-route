bb04659cf2741e205f860a724a43ea534631a843
Inna Aleksenitser <148863587+Inna-B10@users.noreply.github.com>
2025-07-06 01:19:42 +0200

    feat: enhance metadata generation for product and category pages; refactor static params generation for improved performance;

--- src/app/(public)/[category]/[id]/page.jsx
+++ src/app/(public)/[category]/[id]/page.jsx
@@ -1,24 +1,55 @@
 import ScrollToTopButton from '@/components/buttons/ScrollToTopButton'
 import { ProductPage } from '@/components/product-page/ProductPage'
 import { CATEGORIES } from '@/constants/categories'
+import { SITE_NAME } from '@/constants/constants'
 import NotFoundPage from '@/app/not-found'
+import { formatProductTitle } from '@/lib/utils/formatProductTitle'
 import { prepareProductInfo } from '@/lib/utils/prepareProductInfo'
 import { getPreviewProductsData, getProductDataById } from '@/services/server/productsData.service'
 
 export const revalidate = 60 //ISR
 
-export async function generateStaticParams() {
-	const allParams = await Promise.all(
-		CATEGORIES.map(async category => {
-			const products = await getPreviewProductsData(category)
+export async function generateMetadata(props) {
+	const params = await props.params
+	const { category, id } = params
 
-			return products.map(product => ({
-				category,
-				id: product._id.toString()
-			}))
-		})
-	)
-	if (!allParams || allParams.length === 0) return []
+	const product = await getProductDataById(id, category)
+	if (!product) return {}
+
+	const title = formatProductTitle(product)
+
+	return {
+		title: `${title} | ${SITE_NAME}`,
+		description: `Buy ${title} at the best price.`,
+		openGraph: {
+			title,
+			description: `Check out the ${title} available now.`,
+			images: [
+				{
+					url: product['Picture URL'] || '/images/default-no-product.webp',
+					width: 800,
+					height: 600,
+					alt: title
+				}
+			]
+		}
+	}
+}
+
+export async function generateStaticParams() {
+	const allParams = []
+	for (const { slug } of CATEGORIES) {
+		const products = await getPreviewProductsData(slug)
+
+		if (!products || products.length === 0) continue
+
+		const categoryParams = products.map(product => ({
+			category: slug,
+			id: product._id.toString()
+		}))
+
+		allParams.push(...categoryParams)
+	}
 	return allParams.flat()
 }
 
--- src/app/(public)/search/page.jsx
+++ src/app/(public)/search/page.jsx
@@ -1,3 +1,9 @@
+import { NO_INDEX_PAGE } from '@/constants/seo.constants'
+
+export const metadata = {
+	title: 'Search',
+	...NO_INDEX_PAGE
+}
 export default function SearchPage() {
 	return <div>Search</div>
 }
