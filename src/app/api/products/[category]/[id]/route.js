c3931a6353ecbce462ac4e65fe1140ea2268bc96
Inna Aleksenitser <148863587+Inna-B10@users.noreply.github.com>
2025-08-11 12:20:14 +0200

    Generalized API to get product details data

--- src/app/api/products/[category]/[id]/route.js
+++ src/app/api/products/[category]/[id]/route.js
@@ -2,9 +2,9 @@ import { getProductDataById } from '@/services/server/productsData.service'
 
 export async function GET({ params }) {
 	const param = await params
-	const id = param.id
+	const { category, id } = param
 
-	const data = await getProductDataById(id, 'cameras')
+	const data = await getProductDataById(id, category)
 
 	if (!data) {
 		return new Response(JSON.stringify({ message: 'Not Found' }), { status: 404 })
