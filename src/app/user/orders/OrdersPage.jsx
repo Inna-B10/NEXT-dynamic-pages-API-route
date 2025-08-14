8ced1d493b4bd6e0765747d97f5446c44cf66359
Inna Aleksenitser <148863587+Inna-B10@users.noreply.github.com>
2025-08-14 15:06:18 +0200

    working with parsing data and style on OrdersPage

--- src/app/user/orders/OrdersPage.jsx
+++ src/app/user/orders/OrdersPage.jsx
@@ -1,3 +1,54 @@
+import { useUser } from '@clerk/nextjs'
+import { useQuery } from '@tanstack/react-query'
+import toast from 'react-hot-toast'
+import { isDev } from '@/lib/utils/isDev'
+import { ordersService } from '@/services/client/orders.service'
+
 export function OrdersPage() {
-	return <div>OrdersPage</div>
+	const { isLoaded, user } = useUser()
+
+	const {
+		data: orders = [],
+		isLoading
+		// refetch
+	} = useQuery({
+		queryKey: ['orders', user?.id],
+		queryFn: () => (user?.id ? ordersService.getOrdersByUserId(user.id).then(res => res.data) : []),
+		enabled: isLoaded && !!user?.id,
+		onError: error => {
+			toast.error('Error loading orders')
+			if (isDev()) console.error('Error fetching orders:', error)
+		},
+		refetchOnWindowFocus: true //auto-refresh on returning to tab
+	})
+	console.log(orders)
+	return (
+		<>
+			<h1>My Orders</h1>
+			{!isLoading &&
+				(!orders.length ? (
+					<div>No orders</div>
+				) : (
+					orders.map(order => {
+						const date = new Date(order.createdAt)
+						return (
+							<div
+								key={order._id}
+								className='w-full flex flex-col border rounded mb-4 p-4 gap-y-4'
+							>
+								<div className='flex justify-between items-center gap-4'>
+									<span>Order ID: {order._id}</span>
+									<span>Status: {order.status || 'Unknown'}</span>
+								</div>
+								<div className='flex justify-between items-center gap-4'>
+									<span>Order Date: {date.toLocaleDateString()}</span>
+									<span>Items count: {order.items.length}</span>
+									<span>Total Price: {order.totalPrice}</span>
+								</div>
+							</div>
+						)
+					})
+				))}
+		</>
+	)
 }
